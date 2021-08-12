83424f06928484d3bee2e9fcdedbeab85908eabc
Lucas Machado <lucasml.bh@gmail.com>
2021-08-12 19:51:31 -0300

    Identando e adicionando legenda de saldo

--- main.go
+++ main.go
@@ -1,54 +1,81 @@
 package main
 
 import (
- "fmt"
+	"fmt"
 )
 
 func main() {
-saldo := 0
-operacao := "debito"
-valor := 10
+	saldo := 0
 
-saldo,erro:=calculasaldo(operacao, valor, saldo)
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
+	operacao := "debito"
+	valor := 10
 
-operacao = "credito"
-valor = 5
-saldo,erro=calculasaldo(operacao, valor, saldo)
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
+	saldo, erro := calculasaldo(operacao, valor, saldo)
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println(saldo)
+	}
 
-operacao = "debito"
-valor = 2
-saldo,erro=calculasaldo(operacao, valor, saldo)
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
+	operacao = "credito"
+	valor = 5
+	saldo, erro = calculasaldo(operacao, valor, saldo)
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println(saldo)
+	}
 
-operacao = "credito"
-valor = 20
-saldo,erro=calculasaldo(operacao, valor, saldo)
+	operacao = "debito"
+	valor = 2
+	saldo, erro = calculasaldo(operacao, valor, saldo)
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println("Saldo: ", saldo)
+	}
 
+	operacao = "credito"
+	valor = 20
+	saldo, erro = calculasaldo(operacao, valor, saldo)
 
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println("Saldo: ", saldo)
+	}
 
-operacao = "despesa"
-valor = 10
+	operacao = "despesa"
+	valor = 10
+	saldo, erro = calculasaldo(operacao, valor, saldo)
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println("Saldo: ", saldo)
+	}
 
-saldo,erro=calculasaldo(operacao, valor, saldo)
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
-
-operacao = "receita"
-valor = 5
-saldo,erro=calculasaldo(operacao, valor, saldo)
-if ( erro != "") {fmt.Println(erro)}else{fmt.Println(saldo)}
-
+	operacao = "receita"
+	valor = 5
+	saldo, erro = calculasaldo(operacao, valor, saldo)
+	if erro != "" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println("Saldo: ", saldo)
+	}
 }
 
-func calculasaldo(operacao string, valor int, saldoatual int) (int,string){
-if (operacao == "debito" || operacao == "despesa"){
-return saldoatual + valor,""}
+func calculasaldo(operacao string, valor int, saldoatual int) (int, string) {
+	if operacao == "debito" || operacao == "despesa" {
+		return saldoatual + valor, ""
+	}
 
-if (operacao == "credito" || operacao == "receita"){
-if (valor <= saldoatual){
-return saldoatual - valor,""}else {return saldoatual,"Cê tá sem dinheiro tião"}
+	if operacao == "credito" || operacao == "receita" {
+		if valor <= saldoatual {
+			return saldoatual - valor, ""
+		} else {
+			return saldoatual, "Cê tá sem dinheiro tião"
+		}
+	}
+
+	return saldoatual, ""
 }
-
-return saldoatual,""}
